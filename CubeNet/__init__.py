4083525b670a5a0e56282a7b7f07a78379c58948
GuoyiChen <57277998+Billy-Chen0327@users.noreply.github.com>
2022-07-20 13:02:15 +0800

    Add files via upload

--- CubeNet/__init__.py
+++ CubeNet/__init__.py
@@ -1,12 +1,14 @@
 import os
 import torch
 import CubeNet.net
-import CubeNet.Picker
+import CubeNet.train
 import CubeNet.config
+import CubeNet.picker
 
 network = CubeNet.net.UNet(); network.load_state_dict(torch.load(os.path.join(os.path.split(CubeNet.__file__)[0],'Para.pt')));
-picker = CubeNet.Picker.IrrPicker(arr_info=CubeNet.config.arr_info,
+picker = CubeNet.picker.IrrPicker(arr_info=CubeNet.config.arr_info,
                                   para_path = os.path.join(os.path.split(CubeNet.__file__)[0],'Para.pt'),
                                   net = network,
                                   predict_batch = CubeNet.config.basic_info['batch_size'],
-                                  device = CubeNet.config.basic_info['device'])
+                                  device = CubeNet.config.basic_info['device']);
+start_train = CubeNet.train.train;
